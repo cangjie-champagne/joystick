e82108c4079ae0e6503affb4df6d57a687443b0e
João Pedro <joaopjt@icloud.com>
2023-09-13 00:46:55 -0300

    added f() method values

--- src/index.js
+++ src/index.js
@@ -300,7 +300,7 @@ class Joystick {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['twenty'],
       web: []
     };
     
@@ -316,19 +316,19 @@ class Joystick {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['dagger-axe'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['day'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['female'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['mountain'];
           break;
       }
     }
